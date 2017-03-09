8c89433dc959a179cecc97076c988df119517e10
Edson D. Moreno S <diego2816@gmail.com>
2017-03-08 23:23:55 -0430

    Agrgada una funcion

--- Principal.cpp
+++ Principal.cpp
@@ -2,8 +2,14 @@
 
 using namespace std;
 
+void unaFuncion();
 int main(int argc, char** argv) {
 	cout "Hola Mundo"<<endl;
 	cin.get();
 	return 0;
+}
+
+void unaFuncion()
+{
+	cout<<"El codigo de una funcion";
 }
